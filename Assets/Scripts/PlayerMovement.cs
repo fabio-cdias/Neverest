c75cca02361d075f35022b88b7a2decaa18676e9
Fabio-CDias <f92dias@gmail.com>
2023-09-02 05:15:18 -0300

    First commit,Animation and Movement feature

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Animator anim;
+
     void Start()
     {
-
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        bool w_press = Input.GetKey("w");
+        bool isJogging = anim.GetBool("isJogging");
+        Debug.Log(isJogging);
+        if (w_press && !isJogging){
+            anim.SetBool("isJogging",true);
+        }
+        if (!w_press && isJogging){
+            anim.SetBool("isJogging",false);
+        }
     }
 }
